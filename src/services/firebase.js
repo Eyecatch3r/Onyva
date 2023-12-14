9b9771b356b9893689c10d90d5653c7d619d5549
Aldo Costa <aldo.costa@web.de>
2023-12-14 23:46:47 +0100

    -moved config info to .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-    apiKey: "AIzaSyBs6Qq_QEUh46M-0LSwcQDb91_VhcWdv0g",
-    authDomain: "onyvan.firebaseapp.com",
-    projectId: "onyvan",
-    storageBucket: "onyvan.appspot.com",
-    messagingSenderId: "630667295625",
-    appId: "1:630667295625:web:0b027cea0ee6020a1cff58",
-    measurementId: "G-6HKLK71BYS"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
